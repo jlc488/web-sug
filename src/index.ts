d50324b8f612cdea5edd42c5904133c2c9694636
jlc488 <jlc488@gmail.com>
2018-12-26 02:20:43 +0800

    update gitignore, add HMR

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 import app from './app'
 import * as fs from 'fs'
+import { Crawler } from './server/crawler'
 // import * as spdy from 'spdy'
 
 // const options = {
 //   key: fs.readFileSync(__dirname + '/certs/server.key'),
 //   cert: fs.readFileSync(__dirname + '/certs/server.crt'),
 // }
-
 const PORT = 3000
-
 // const server = spdy.createServer(options, app)
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`The server is listening on port ${PORT}`)
+  new Crawler('http://google.com').goFetch()
 })
+
+declare const module: any
+
+if (module.hot) {
+  module.hot.accept()
+
+  module.hot.dispose(() => server.close())
+}
