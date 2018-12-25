939866b0f9748ff1158c78ec41fc53fa6e04ba05
jlc488 <jlc488@gmail.com>
2018-12-26 02:09:31 +0800

    adding dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import * as helmet from 'helmet'
 
 class App {
   public app: express.Application
 
   constructor() {
     this.app = express()
+    this.app.use(helmet())
     this.config()
   }
 
--- src/server/crawler.ts
+++ src/server/crawler.ts
@@ -1,3 +1,28 @@
+import * as cheerio from 'cheerio'
+import * as urlParse from 'url-parse'
+import * as request from 'request'
+
 export class Crawler {
-    
+  url: string
+
+  constructor(url) {
+    this.url = url
+  }
+
+  goFetch() {
+    console.log(`Visiting Page ${this.url}`)
+
+    request(this.url, (err, response, body) => {
+      if (err) {
+        console.log(`Error : ${err}`)
+      }
+
+      console.log(`Status Code : ${response.statusCode}`)
+
+      if (response.statusCode === 200) {
+        let $ = cheerio.load(body)
+        console.log('Page title :' + $('title').text())
+      }
+    })
+  }
 }
